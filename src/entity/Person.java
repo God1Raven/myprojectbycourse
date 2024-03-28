3ffda3fd59def894666a351db917c60747a423b0
God1Raven <Noymov@mail.ru>
2024-03-28 19:00:42 +0100

    Lets go

---
+++ src/entity/Person.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Person {
+    private final int id;
+    private final int age;
+    private final String name;
+
+    public Person(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+}
